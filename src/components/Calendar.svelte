9b601da90ba6520677da8a3b536d2edcc1cdbb75
Meph1sto666 <101014274+Meph1sto666@users.noreply.github.com>
2023-07-18 22:08:12 +0200

    I hate css

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -1,17 +1,15 @@
 <title>CALENDAR</title>
 
 <script lang="ts">
-    import { goto } from "$app/navigation";
-	import { getGameTime, getDaysInMonth, isSameDay, weeksSinceMonthStart, timeToColor, isInSameWeek } from "$lib";
+	import { getDaysInMonth } from "$lib";
     import data from "../data/calendar/events.json";
 	import { AkEvent } from "../lib/types/event";
-	import Month from "./calendar/month.svelte";
+	import Month from "./calendar/Month.svelte";
 	
-	var events: AkEvent[] = [];
+	const events: AkEvent[] = [];
 	for (let i of data.future) {
 		events.push(new AkEvent(i.start, i.end, i.id));
 	}
-
 
 	function getMonthsBetweenDates(startDate: Date, endDate: Date): number {
 		let diffMonths = (endDate.getFullYear() - startDate.getFullYear()) * 12;
@@ -20,7 +18,6 @@
 		return diffMonths <= 0 ? 0 : diffMonths;
 	}
 
-	const TIME = getGameTime();
 	var MONTHS = Array.from(
 		{
 			length:
@@ -40,55 +37,12 @@
 			return new Date(m.getFullYear(), m.getMonth(), m.getDate() + i);
 		});
 	});
-
-	function getOverlapTags(e:AkEvent, d:Date):string {
-		for (let i of events) {
-			if (e==i) continue
-			if (!(e.isWithinEvent(d) && i.isWithinEvent(d))) continue; // !(e.isWithinEvent(i.getStart()) || e.isWithinEvent(i.getEnd())) && || i.isWithinEvent(e.getStart()) || i.isWithinEvent(e.getEnd())
-			if (i.getStart() <= e.getStart()) return " overlap-bottom"
-			else return " overlap-top"
-		}
-		return ""
-	}
-
-	function getClasses(d:Date, e:AkEvent) {
-		return `event${isSameDay(d,e.getStart())?' event-start':''}${isSameDay(d,e.getEnd())?' event-end':''}${getOverlapTags(e,d)}`
-	}
-	
-	function redirectEvent(event:Event) {
-		// @ts-ignore
-		goto(`/events/${event.target?.dataset.event_id}`);
-	}
-
 </script>
 
 <main>
 	<div id="cal-container">	
 		{#each MONTHS as m}
-			<Month></Month>
-			<section class="{m[0].getDay()!=0?'section-shift':''}">
-				<div class="cal-grid" id={String(m[0].getMonth())}>
-					{#each m as d}
-					<div class="cal-grid-day{isSameDay(TIME, d)?' today':''}" id={String(d.getDate())} style="{(d.getDate()==1)? '--col-start:' + (d.getDay()+1):''}">
-						{d.getDate()}
-					</div>
-					{/each}
-				</div>
-				<div class="overlay">
-					{#each events as e}
-						{#if e.getStart().getMonth() === m[0].getMonth() || e.getEnd().getMonth() == m[0].getMonth()}
-							{#each m as d}
-								{#if e.isWithinEvent(d) && ((d.getDay()==0) || isSameDay(d, e.getStart()) || (isSameDay(d, e.getEnd()) && !(isInSameWeek(d,e.getEnd()))))}
-									<div class="{getClasses(d,e)}" on:click={redirectEvent} data-event_id="{e.getId()}" role="none"
-										style="--row: {weeksSinceMonthStart(d)}; --col-start: {isSameDay(d,e.getStart())?(d.getDay()+1)*2:d.getDay()*2+1}; --col-end: {isInSameWeek(d,e.getEnd())?(e.getEnd().getDay()+1)*2:7*2+1}; --colors: {timeToColor(e.getStart())}, {timeToColor(e.getEnd())}">
-											{(isInSameWeek(d,e.getStart()))?e.getDisplayName():''}
-									</div>
-								{/if}
-							{/each}
-						{/if}
-					{/each}
-				</div>
-			</section>
+			<Month m={m} events={events}></Month>
 		{/each}
 	</div>
 </main>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang='ts'>
+	import { AppBar, AppShell } from "@skeletonlabs/skeleton";
     import { getGameTime } from "$lib";
 	// The ordering of these imports is critical to your app working properly
-	import './../styles/themes/dark.postcss';
+	import '../styles/themes/dark.postcss';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	
+	import '../styles/calendar.postcss';
+	import Calendar from "../components/Calendar.svelte";
 
 	let time = getGameTime()
 	function updateTime() {
@@ -13,12 +17,33 @@
 	}
 	const interval = setInterval(updateTime, 1000);
 </script>
-<nav>
-	<div id="nav-back" />
-	<div id="nav-home" />
-	<div id="en-game-time-clock">
-		//{time.getFullYear().toString().padStart(4,"0")}/{time.getMonth().toString().padStart(2,"0")}-{time.getDate().toString().padStart(2,"0")
-	}////{time.getHours().toString().padStart(2,"0")}-{time.getMinutes().toString().padStart(2,"0")}-{time.getSeconds().toString().padStart(2,"0")}//
-	<div>
-</nav>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<div  class="text-4xl">
+					<span class="first-letter: text-secondary-600 p-0 m-0">AR</span>
+					<span class="first-letter: text-primary-600 p-0 m-0">K</span>
+					<span class="first-letter: text-tertiary-600 p-0 m-0">ALENDAR</span>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<span class="">
+					//{time.getFullYear().toString().padStart(4,"0")}-{time.getMonth().toString().padStart(2,"0")}-{time.getDate().toString().padStart(2,"0")}////{time.getHours().toString().padStart(2,"0")}-{time.getMinutes().toString().padStart(2,"0")}-{time.getSeconds().toString().padStart(2,"0")}//
+				</span>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<svelte:fragment slot="sidebarRight">
+		<Calendar></Calendar>
+	</svelte:fragment>
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
 <slot></slot>
