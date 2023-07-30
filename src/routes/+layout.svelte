e9397e373913af7688148386ab77fe0a0621e1f8
Meph1sto666 <101014274+Meph1sto666@users.noreply.github.com>
2023-07-30 21:14:39 +0200

    added style imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang='ts'>
 	import { AppBar, AppShell } from "@skeletonlabs/skeleton";
     import { getGameTime } from "$lib";
-	// The ordering of these imports is critical to your app working properly
+
 	import '../styles/themes/dark.postcss';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	
 	import '../styles/calendar.postcss';
